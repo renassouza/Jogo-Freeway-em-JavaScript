518d5e2359065602cd8f9933207f45dee06607b5
Renata Souza <renataoliveira1497@gmail.com>
2021-09-08 22:37:35 -0300

    Create imagens.js

---
+++ imagens.js
@@ -0,0 +1,16 @@
+let imagemDaEstrada;
+let imageDoPato;
+let imagemDoCar1;
+let imagemDocar2;
+let imagemDocar3;
+
+function preload(){
+    imagemDaEstrada = loadImage("imagens/estrada2.png");
+    imagemDoPato = loadImage("imagens/pato.png");
+    imagemDoCar1 = loadImage("imagens/car1.png");
+    imagemDoCar2 = loadImage("imagens/car2.png");
+    imagemDoCar3 = loadImage("imagens/car3.png");
+    imagemDoCars = [imagemDoCar1, imagemDoCar2,imagemDoCar3, imagemDoCar1, imagemDoCar2, imagemDoCar3];
+  
+}
+
